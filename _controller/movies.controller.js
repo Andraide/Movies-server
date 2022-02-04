83700ec964f2ec023985976a7f5a8721ae462810
Cristobal Andrade Steil <cristobalsteil@MacBook-Pro-de-Cristobal.local>
2022-02-04 15:10:48 -0300

    Initial

--- _controller/movies.controller.js
+++ _controller/movies.controller.js
@@ -1,18 +1,37 @@
 const Router = require('koa-router')
 const moviesService = require('../_services/movies.service')
 const router = new Router()
+const Joi = require('joi');
+const mongoose = require('mongoose');
 
 router.get('/', XD )
+router.get('/check', checkDb)
+
+const schema = Joi.object({
+    year: Joi.number()
+        .min(1800)
+        .max(2050)
+        .required(),
+
+})
+
+async function checkDb(ctx, next)
+{
+    ctx.body = mongoose.STATES[mongoose.connection.readyState] ;
+}
 
 async function XD(ctx, next){
     try 
     {
-        const vikings = await moviesService.getVikings()
-        ctx.body = vikings
+        console.log("Header", ctx.get('Year'), schema.validate(ctx.get('Year')) )
+        const params = ctx.request.query
+        const title = params.title
+        const vikings = await moviesService.getVikings(title)
+        ctx.body = vikingsE
     }
     catch (err) 
     {
-        ctx.status = err
+        //ctx.status = err
         ctx.body = "There was an error" + " " + err
     }   
 };

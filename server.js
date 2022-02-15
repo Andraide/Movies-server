eb2ffe1ff3a74e9af3b9f165439e934501821a5d
Cristobal_Andrade <cristobal.andrade.steil@gmail.com>
2022-02-15 18:21:51 -0300

    refactor: rm error-handler

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = new Koa()
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger')
 const router = require('./_controller/movies.controller')
-const errorHandler = require('./helpers/error-handler')
 const swagger = require("swagger2");
 const { ui, validate } = require("swagger2-koa");
 const swaggerDocument = swagger.loadDocumentSync("api.yaml");
@@ -12,12 +11,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test')
     .catch(error => console.log("Error connecting to Db", error))
 
-//app.use(ui(swaggerDocument, "/swagger"))
 app.use(logger())
 app.use(bodyParser())
-//app.use(router.routes())
-//app.use(errorHandler);
-//app.use(router.allowedMethods())
 
 app.on('error', (err, ctx) => {
     console.error("server error",err)
